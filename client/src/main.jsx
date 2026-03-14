6a281a305c7c00e6e1ad0728b1bebbc8e9cd064f
Aditya <2k23.csds2312133@gmail.com>
2026-03-14 19:17:23 +0530

    feat: integrate ClerkProvider for authentication

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,19 @@ import {createRoot} from "react-dom/client"
 import "./index.css"
 import App from "./App.jsx"
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if(!PUBLISHABLE_KEY){
+    throw new Error('Missing Publishable key')
+}
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-        <App></App>
-    </BrowserRouter>
+    <ClerkProvider>
+        <BrowserRouter>
+            <App></App>
+        </BrowserRouter>
+    </ClerkProvider>
+    
 )
