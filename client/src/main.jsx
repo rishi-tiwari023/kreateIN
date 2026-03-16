6535161597142a6d46648fb77ee7d23ce74ecca9
Rishi Tiwari <umesh896073@gmail.com>
2026-03-16 23:28:58 +0530

    chore: Update remaining files, assets, and project configurations

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx"
 import { BrowserRouter } from "react-router-dom";
 import { ClerkProvider } from '@clerk/clerk-react'
 
+//Import publishable key from environment variables
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if(!PUBLISHABLE_KEY){
@@ -11,9 +12,9 @@ if(!PUBLISHABLE_KEY){
 }
 
 createRoot(document.getElementById('root')).render(
-    <ClerkProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
         <BrowserRouter>
-            <App></App>
+            <App/>
         </BrowserRouter>
     </ClerkProvider>
     
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Testimonial from '../components/Testimonials'
+import AiTools from '../components/AiTools'
+import Plan from '../components/Plan'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
+      <AiTools/>
+      <Testimonial/>
+      <Plan/>
     </div>
   )
 }
