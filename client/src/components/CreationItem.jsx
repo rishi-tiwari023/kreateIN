fcf0997255be0bf89f6aa3ae973aaba7b522b141
Aditya <2k23.csds2312133@gmail.com>
2026-03-17 23:34:34 +0530

    feat: add CreationItem component for displaying user creations

--- client/src/components/CreationItem.jsx
+++ client/src/components/CreationItem.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import ReactMarkdown from 'react-markdown'
 
 const CreationItem = ({ item }) => {
+
+    const [expanded,setExpanded] = useState(false)
+
   return (
-    <div className='p-4 max-w-5xl text-sm bg-white border border-gray-200 rounded-lg cursor-pointer'>
+    <div onClick={()=> setExpanded(!expanded)} className='p-4 max-w-5xl text-sm bg-white border border-gray-200 rounded-lg cursor-pointer'>
       <div className='flex justify-between items-center gap-4'>
         <div>
           <h2 className='font-medium'>{item.prompt}</h2>
@@ -15,6 +19,24 @@ const CreationItem = ({ item }) => {
           {item.type}
         </button>
       </div>
+      {
+        expanded && (
+            <div>
+                {item.type === 'image' ? (
+                    <div>
+                        <img src={item.content} alt="image" className='mt-3 w-full max-w-md'/>
+                    </div>
+                ) : (
+                    <div className='mt-3 h-full overflow-y-scroll text-sm
+                    text-slate-700'>
+                        <div>
+                            {item.content}
+                        </div>
+                    </div>
+                )}
+            </div>
+        )
+      }
     </div>
   )
 }
