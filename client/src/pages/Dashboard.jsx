bdbb9d2847322d295636bdec563311e6c5c1a658
Aditya <2k23.csds2312133@gmail.com>
2026-03-17 22:46:33 +0530

    feat: add active plan card and recent creations section to dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { dummyCreationData } from '../assets/assets'
-import { Sparkles } from 'lucide-react';
+import { Gem, Sparkles } from 'lucide-react';
+import { Protect } from '@clerk/clerk-react'
+
 
 const Dashboard = () => {
   const [creations , setCreations] = useState([])
@@ -28,7 +30,27 @@ const Dashboard = () => {
             <Sparkles className='w-5 text-white'/>
           </div>
         </div>
+
+        {/*Active Plan Card*/}
+        <div className='flex justify-between items-center w-72
+        p-4 px-6 bg-white rounded-xl border border-gray-200'>
+          <div className='text-slate-600'>
+            <p className='text-sm'>Active Plan</p>
+            <h2 className='text-xl font-semibold'>
+              <Protect plan='premium' fallback='Free'>Premium</Protect>
+            </h2>
+          </div>
+          <div className='w-10 h-10 rounded-lg bg-gradient-to-br from-[#FF61C5]
+          to-[#9E53EE] text-white flex justify-center items-center'>
+            <Gem className='w-5 text-white'/>
+          </div>
+        </div>
       </div>
+
+      <div className='space-y-3'>
+        <p className='mt-6 mb-4'> Recent Creations </p>
+      </div>
+
     </div>
   )
 }
