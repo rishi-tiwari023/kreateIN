69e5dab78a4a60b8aaa4499afd0a8fc2d34a8a97
Aditya <2k23.csds2312133@gmail.com>
2026-03-17 21:49:14 +0530

    feat: add protected layout with Clerk auth and sign-in fallback

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -2,20 +2,36 @@ import React , {useState} from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import {assets} from '../assets/assets.js';
 import { Menu, X } from 'lucide-react';
+import Sidebar from '../components/Sidebar.jsx';
+import {SignIn , useUser } from '@clerk/clerk-react';
 
 const Layout = () => {
   const navigate = useNavigate()
   const [sidebar,setSidebar] = useState(false)
-  return (
+  const {user} = useUser()
+
+
+  return user ? (
     <div className='flex flex-col items-start justify-start h-screen'>
-      <nav>
-        <img src={assets.logo} onClick={() => navigate('/')} />
+      <nav className='w-full px-8 h-14 flex items-center justify-between
+        border-b border-gray-200'>
+        <img src={assets.logo} onClick={() => navigate('/')} className="w-32 cursor-pointer"/>
         {
-          sidebar ? <X className='w-6 h-6 text-gray-600 sm:hidden'/>
-        : <Menu className='w-6 h-6 text-gray-600 sm:hidden' />
+          sidebar ? <X onClick={()=> setSidebar(false)} className='w-6 h-6 text-gray-600 sm:hidden'/>
+        : <Menu onClick={()=> setSidebar(true)} className='w-6 h-6 text-gray-600 sm:hidden' />
         }
       </nav>
-      <Outlet />
+      <div className='flex-1 w-full flex h-[calc(100vh-56px)]'>
+        <Sidebar sidebar={sidebar} setSidebar={setSidebar}/>
+        <div className='flex-1 bg-[#F4F7FB]'>
+          <Outlet/>
+        </div>
+      </div>
+      
+    </div>
+  ) : (
+    <div className='flex items-center justify-center h-screen'>
+      <SignIn/>
     </div>
   )
 }
