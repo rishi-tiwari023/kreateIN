c3f7819ac0daa0b8e5b4147276b51610fc361723
Rishi Tiwari <umesh896073@gmail.com>
2026-03-21 17:10:48 +0530

    feat(server): implement image background and object removal using cloudinary and multer

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { auth } from "../middlewares/auth.js";
-import { generateArticle, generateBlogTitle, generateImage } from "../controllers/aiController.js";
+import { generateArticle, generateBlogTitle, generateImage, removeImageBackground, removeImageObject } from "../controllers/aiController.js";
+import { upload } from "../configs/multer.js";
 
 export const aiRouter = express.Router();
 
 aiRouter.post("/generate-article", auth, generateArticle);
 aiRouter.post("/generate-blog-title", auth, generateBlogTitle);
 aiRouter.post("/generate-image", auth, generateImage);
+aiRouter.post("/remove-bg", auth, upload.single('image'), removeImageBackground);
+aiRouter.post("/remove-object", auth, upload.single('image'), removeImageObject);
