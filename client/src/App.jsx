636aaa855f3a99966fe044b575dca46b513aeb52
Aditya <2k23.csds2312133@gmail.com>
2026-03-21 22:54:42 +0530

    update: Updated App.jsx accordingly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,19 +12,13 @@ import ReviewResume from './pages/ReviewResume'
 import Community from './pages/Community'
 import { useAuth } from '@clerk/clerk-react'
 import { useEffect } from 'react'
+import {Toaster} from 'react-hot-toast'
 
 const App = () => {
-  const { getToken } = useAuth()
-  useEffect(() => {
-    getToken().then((token) => 
-      { console.log("User token:", token) })
-    .catch((error) => {
-      console.error("Error fetching token:", error);
-    });
 
-  },[])
   return (
     <div className='min-h-screen'>
+      <Toaster/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/ai' element={<Layout/>}>
