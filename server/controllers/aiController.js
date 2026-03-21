9dfa84316d705df89426de9789da6c22b938c823
Aditya <2k23.csds2312133@gmail.com>
2026-03-21 21:59:03 +0530

    feat: add AI resume review and image object removal features

--- server/controllers/aiController.js
+++ server/controllers/aiController.js
@@ -3,6 +3,8 @@ import sql from "../configs/db.js";
 import { clerkClient } from "@clerk/express";
 import { v2 as cloudinary } from 'cloudinary';
 import axios from "axios";
+import fs from 'fs';
+import * as pdf from "pdf-parse";
 
 const AI = new OpenAI({
     apiKey: process.env.GEMINI_API_KEY,
@@ -31,7 +33,7 @@ export const generateArticle = async (req,res)=>{
 
         const content=response.choices[0].message.content;
 
-        await sql`INSERT INTO creations (user_id, prompt , content , type) 
+        await sql`INSERT INTO Creations (user_id, prompt , content , type) 
         VALUES (${userId}, ${prompt}, ${content}, 'article')`;
 
         if (plan !== 'premium' ){
@@ -70,7 +72,7 @@ export const generateBlogTitle = async (req,res)=>{
 
         const content=response.choices[0].message.content;
 
-        await sql`INSERT INTO creations (user_id, prompt , content , type) 
+        await sql`INSERT INTO Creations (user_id, prompt , content , type) 
         VALUES (${userId}, ${prompt}, ${content}, 'blog-title')`;
 
         if (plan !== 'premium' ){
@@ -164,7 +166,7 @@ export const removeImageObject = async (req,res)=>{
     try{
         const {userId} = req.auth();
         const { object } = req.body;
-        const image = req.file;
+        const {image} = req.file;
         const plan = req.plan;
 
 
@@ -181,6 +183,51 @@ export const removeImageObject = async (req,res)=>{
 
         await sql`INSERT INTO creations (user_id, prompt , content , type) 
         VALUES (${userId}, ${`Removed ${object} from image`}, ${imageUrl}, 'image')`;
+
+        res.json({ success : true , content : imageUrl })
+
+
+    }catch(error)
+    {
+        console.log(error.message);
+        res.json({ success : false , message : error.message })
+    }
+}
+
+
+export const resumeReview = async (req,res)=>{
+    try{
+        const {userId} = req.auth();
+    
+        const resume = req.file;
+        const plan = req.plan;
+
+
+        if(plan !== 'premium'){
+            return res.json({ success : false  ,
+                 message : "This Feature is only available for premium subscription. Upgrade to continue."})
+        }
+        
+        if(resume.size >  5*1024*1024){
+            return res.json({success : false, message : "Resume file size exceeds allowed size of 5mb."})
+        }
+
+        const dataBuffer =  fs.readFileSync(resume.path)
+        const pdfData = await pdf(dataBuffer)
+
+        const prompt = `Review the following resume and provide constructive feedback
+        on its strengths, weaknesses , and areas for improvement. Resume Content:\n\n${pdfData.text}`
+
+        const response = await AI.chat.completions.create({
+            model: "gemini-3-flash-preview",
+            messages: [{role: "user", content: prompt, } ],temperature: 0.7,
+            max_tokens: 1000,
+        });
+
+        const content=response.choices[0].message.content;
+
+        await sql`INSERT INTO creations (user_id, prompt , content , type) 
+        VALUES (${userId}, 'Review the uploaded resume', ${content}, 'resume-review')`;
 
         res.json({ success : true , content : imageUrl })
 
