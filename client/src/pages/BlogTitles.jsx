96bc5a6dc14e08b06b588be87162d143fed7c23b
Rishi Tiwari <umesh896073@gmail.com>
2026-03-22 16:06:37 +0530

    feat: implement API integration and content rendering for BlogTitles and WriteArticle pages

--- client/src/pages/BlogTitles.jsx
+++ client/src/pages/BlogTitles.jsx
@@ -1,14 +1,41 @@
-import {Hash, Sparkles } from 'lucide-react'
+import { Hash, Sparkles } from 'lucide-react'
 import React from 'react'
 import { useState } from 'react'
+import toast from 'react-hot-toast';
+import axios from 'axios'
+import Markdown from 'react-markdown'
+import { useAuth } from '@clerk/clerk-react';
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 const BlogTitles = () => {
   const blogCategories = ['General', 'Technology', 'Business', 'Health',
     'LifeStyle', 'Education', 'Travel', 'Food', 'Entertainment', 'Sports']
   const [selectedCategory, setSelectedCategory] = useState(blogCategories[0])
   const [input, setInput] = useState('')
+  const [loading, setLoading] = useState(false)
+  const [content, setContent] = useState('')
+
+  const { getToken } = useAuth();
   const onSubmitHandler = async (e) => {
     e.preventDefault();
+    try {
+      setLoading(true)
+      const prompt = `Generate catchy blog titles for the keyword ${input} in the
+       category ${selectedCategory}.x`
+      const { data } = await axios.post('/api/ai/generate-blog-title', { prompt }, {
+
+        headers: { Authorization: `Bearer ${await getToken()}` }
+      })
+      if (data.success) {
+        setContent(data.content)
+      } else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.response.data.message)
+    }
+    setLoading(false)
   }
   return (
     <div className='h-full overflow-y-scroll p-6 flex items-start flex wrap gap-4 text-slate-700'>
@@ -19,7 +46,8 @@ const BlogTitles = () => {
           <Sparkles className='w-6 text-[#8E37EB]' />
           <h1 className='text-xl font-semibold'>AI Title Generator</h1>
         </div>
-        <p className='text-xl font-semibold'>Keyword</p>
+        <br />
+        <p className='text-sm font-semibold'>Keyword</p>
         <input onChange={(e) => setInput(e.target.value)} value={input} type="text" className='w-full p-2 px-3 mt-2 outline-none text-sm
         rounded-md border border-gray-300' placeholder='Artificial Intelligence, Machine Learning, Travel, USA, LPG, etc..... '
           required />
@@ -33,10 +61,11 @@ const BlogTitles = () => {
           ))}
         </div>
         <br />
-        <button className='w-full flex justify-center items-center gap-2
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2
         bg-gradient-to-r from-[#C341F6] to-[#8E37EB] text-white px-4 py-2 mt-6
         text-sm rounded-lg cursor-pointer'>
-          <Hash className='w-5' />
+          {loading ? <span className='w-4 h-4 my-1 rounded-full border-2
+            border-t-transparent animate-spin'></span> : <Hash className='w-5' />}
           Generate Title
         </button>
 
@@ -48,14 +77,22 @@ const BlogTitles = () => {
           <Hash className='w-5 h-5 text-[#8E37EB]' />
           <h1 className='text-xl font-semibold'>Generated Titles</h1>
         </div>
-        <div className='flex-1 flex justify-center items-center'>
-          <div className='text-sm flex flex-col items-center gap-5
-          text-gray-400'>
-            <Hash className='w-9 h-9' />
-            <p>Enter a topic and click "Generate Title" to get started</p>
+        {!content ? (
+          <div className='flex-1 flex justify-center items-center'>
+            <div className='text-sm flex flex-col items-center gap-5
+            text-gray-400'>
+              <Hash className='w-9 h-9' />
+              <p>Enter a topic and click "Generate Title" to get started</p>
+            </div>
           </div>
-
-        </div>
+        ) : (
+          <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600'>
+            <div className='reset-tw'>
+              <Markdown>{content}</Markdown>
+            </div>
+          </div>
+        )
+        }
 
       </div>
     </div>
--- client/src/pages/WriteArticle.jsx
+++ client/src/pages/WriteArticle.jsx
@@ -4,6 +4,8 @@ import { useState } from 'react'
 import axios from 'axios'
 import { useAuth } from '@clerk/clerk-react';
 import toast from 'react-hot-toast';
+import Markdown from 'react-markdown';
+
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
@@ -28,9 +30,10 @@ const WriteArticle = () => {
 
     try{
       setLoading(true)
-      const prompt = `Write an article about ${input} in ${selectedLength.text}`
+      const prompt = `Write a comprehensive and engaging article about ${input} in approximately ${selectedLength.text}. Format the response in Markdown with appropriate headings and sections. Return ONLY the article content, without any introductory text.`
 
       const {data} = await axios.post('/api/ai/generate-article' , {prompt,length:selectedLength.length} , {
+
         headers: {Authorization: `Bearer ${await getToken()}`}
       })
 
@@ -42,7 +45,7 @@ const WriteArticle = () => {
 
     }
     catch(error){
-       toast.error(data.message)
+       toast.error(error.response.data.message)
     }
     setLoading(false)
   }
@@ -69,10 +72,14 @@ const WriteArticle = () => {
           ))}
         </div>
         <br />
-        <button className='w-full flex justify-center items-center gap-2
+        <button disabled={loading} className='w-full flex justify-center items-center gap-2
         bg-gradient-to-r from-[#226BFF] to-[#65ADFF] text-white px-4 py-2 mt-6
         text-sm rounded-lg cursor-pointer'>
-          <Edit className='w-5' />
+          {
+            loading ? <span className='w-4 h-4 my-1 rounded-full border-2
+            border-t-transparent animate-spin'></span>
+            : <Edit className='w-5' />
+          }
             Generate Article
         </button>
 
@@ -84,14 +91,24 @@ const WriteArticle = () => {
           <Edit className='w-5 h-5 text-[#4A7AFF]' />
           <h1 className='text-xl font-semibold'>Generated Article</h1>
         </div>
-        <div className='flex-1 flex justify-center items-center'>
+
+        {!content ? (
+          <div className='flex-1 flex justify-center items-center'>
           <div className='text-sm flex flex-col items-center gap-5
           text-gray-400'>
             <Edit className='w-9 h-9' />
             <p>Enter a topic and click "Generate Article" to get started</p>
           </div>
-
         </div>
+
+        ) : (
+          <div className='mt-3 h-full overflow-y-scroll text-sm text-slate-600'>
+              <div className='reset-tw'>
+                <Markdown>{content}</Markdown>
+              </div>
+          </div>
+        )}
+        
 
       </div>
     </div>
